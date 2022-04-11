60878786f3e5c53de3fa9e6539866c4c89cdc606
jsirianni <joe.sirianni@observiq.com>
2022-04-11 16:55:20 -0400

    minikube ci

---
+++ .github/actions/minikube/minikube.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Assumes pwd == git root
+
+set -e
+
+k8s_version="$1"
+k8s_runtime="$2"
+
+minikube_install() {
+    sudo apt-get update -qq
+    sudo apt-get install -qq -y conntrack
+    curl -s -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube
+}
+
+minikube_start() {
+    minikube start \
+        --driver=docker \
+        --kubernetes-version="$k8s_version" \
+        --container-runtime="$k8s_runtime"
+}
+
+minikube_wait() {
+    node=$(kubectl get node | grep -v NAME | awk '{print $1}')
+    bash ./.github/scripts/k8s_wait_for_node.sh "$node"
+}
+
+minikube_install
+minikube_start
+minikube_wait
