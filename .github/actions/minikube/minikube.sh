c41e7c4a64b7b625479a1fa5d8eac222bd126869
jsirianni <joe.sirianni@observiq.com>
2022-04-11 17:02:18 -0400

    add debug

--- .github/actions/minikube/minikube.sh
+++ .github/actions/minikube/minikube.sh
@@ -9,11 +9,16 @@ k8s_runtime="$2"
 minikube_install() {
     sudo apt-get update -qq
     sudo apt-get install -qq -y conntrack
+
+    echo "Installing Minikube version ${k8s_version}"
+
     curl -s -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
     sudo install minikube-linux-amd64 /usr/local/bin/minikube
 }
 
 minikube_start() {
+    echo "Starting Minikube version ${k8s_version} with runtime ${k8s_runtime}" 
+
     minikube start \
         --driver=docker \
         --kubernetes-version="$k8s_version" \
